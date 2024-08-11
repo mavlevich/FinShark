499828e5a297e3044b9afff38bac5d71871c9360
mavlevich <mavlevichx@gmail.com>
2024-08-11 21:34:08 +0200

    commit

--- FinShark/obj/Debug/net8.0/FinShark.AssemblyInfo.cs
+++ FinShark/obj/Debug/net8.0/FinShark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinShark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d8b7372fe201a112b31df59f8f768ca9110616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97a3c1afd8e5843a3c68fcb821ffc31ae31b106")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinShark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinShark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
